5d2cfecd62a92bf4b2fcfbc75a68855f1a59d71d
Aller Augusto de Souza <allerasouza@gmail.com>
2022-09-10 12:53:04 -0300

    chore: add vpc and security group

--- main.tf
+++ main.tf
@@ -20,3 +20,24 @@ resource "aws_instance" "dev" {
       "Name" = "dev${count.index}"
     }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["191.13.39.108/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
