8f7cb40bdb504cc0586a1dda26aa58fac3a9b2e0
Aller Augusto de Souza <allerasouza@gmail.com>
2022-09-10 16:55:31 -0300

    chore: add new ec2 instance in other aws region

--- main.tf
+++ main.tf
@@ -11,43 +11,60 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
-    count = 3
-    ami = "ami-052efd3df9dad4825"
-    instance_type = "t2.micro"
-    key_name = "terraform-alura"
-    tags = {
-      "Name" = "dev${count.index}"
-    }
-    vpc_security_group_ids = [aws_security_group.acesso-ssh.id]
+  count         = 3
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name      = "terraform-alura"
+  tags = {
+    "Name" = "dev${count.index}"
+  }
+  vpc_security_group_ids = [aws_security_group.acesso-ssh.id]
 }
 
 resource "aws_instance" "dev4" {
-    ami = "ami-052efd3df9dad4825"
-    instance_type = "t2.micro"
-    key_name = "terraform-alura"
-    tags = {
-      "Name" = "dev4"
-    }
-    vpc_security_group_ids = [aws_security_group.acesso-ssh.id]
-    depends_on = [aws_s3_bucket.dev4]
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name      = "terraform-alura"
+  tags = {
+    "Name" = "dev4"
+  }
+  vpc_security_group_ids = [aws_security_group.acesso-ssh.id]
+  depends_on             = [aws_s3_bucket.dev4]
 }
 
 resource "aws_instance" "dev5" {
-    ami = "ami-052efd3df9dad4825"
-    instance_type = "t2.micro"
-    key_name = "terraform-alura"
-    tags = {
-      "Name" = "dev5"
-    }
-    vpc_security_group_ids = [aws_security_group.acesso-ssh.id]
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name      = "terraform-alura"
+  tags = {
+    "Name" = "dev5"
+  }
+  vpc_security_group_ids = [aws_security_group.acesso-ssh.id]
+}
+
+resource "aws_instance" "dev6" {
+  provider      = aws.us-east-2
+  ami           = "ami-0568773882d492fc8"
+  instance_type = "t2.micro"
+  key_name      = "terraform-alura"
+  tags = {
+    "Name" = "dev6"
+  }
+  vpc_security_group_ids = [aws_security_group.acesso-ssh-us-east-2.id]
+  subnet_id              = aws_subnet.my_subnet.id
 }
 
 resource "aws_s3_bucket" "dev4" {
   bucket = "aaslabs-dev4"
 
   tags = {
-    Name        = "aaslabs-dev4"
+    Name = "aaslabs-dev4"
   }
 }
 
--- security-group.tf
+++ security-group.tf
@@ -1,7 +1,4 @@
-# resource "aws_vpc" "main" {
-#   cidr_block = "10.0.0.0/16"
-# }
-
+##### us-east-1 #####
 data "aws_vpc" "main" {
   id = "vpc-0ca5493417621a83b"
 }
@@ -12,13 +9,48 @@ resource "aws_security_group" "acesso-ssh" {
   vpc_id      = data.aws_vpc.main.id
 
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["191.13.39.108/32"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["191.13.39.108/32"]
   }
 
   tags = {
     Name = "ssh"
   }
+}
+
+##### us-east-2 #####
+resource "aws_vpc" "main" {
+  provider   = aws.us-east-2
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+  provider   = aws.us-east-2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "my_subnet"
+  }
+}
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+  provider    = aws.us-east-2
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["191.13.39.108/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+  depends_on = [aws_vpc.main]
 }
